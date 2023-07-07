fcd83741776848ee5a4989997c3681bfb6f7d060
SterzerA <alexander.w.sterzer@googlemail.com>
2023-07-07 11:43:34 +0200

    Initial import of ST BLE_Heartrate project as reference

---
+++ BLE_HeartRate/Debug/Middlewares/STM32_WPAN/ble_gatt_aci.d
@@ -0,0 +1,16 @@
+Middlewares/STM32_WPAN/ble_gatt_aci.o: \
+ C:/Users/alexa/Downloads/en.stm32cubewb-v1-16-0/STM32Cube_FW_WB_V1.16.0/Middlewares/ST/STM32_WPAN/ble/core/auto/ble_gatt_aci.c \
+ C:/Users/alexa/Downloads/en.stm32cubewb-v1-16-0/STM32Cube_FW_WB_V1.16.0/Middlewares/ST/STM32_WPAN/ble/core/auto/ble_gatt_aci.h \
+ C:/Users/alexa/Downloads/en.stm32cubewb-v1-16-0/STM32Cube_FW_WB_V1.16.0/Middlewares/ST/STM32_WPAN/ble/core/auto/ble_types.h \
+ ../../../../../../../Middlewares/ST/STM32_WPAN/ble/core/template/ble_const.h \
+ ../../../../../../../Middlewares/ST/STM32_WPAN/ble/core/ble_std.h \
+ ../../../../../../../Middlewares/ST/STM32_WPAN/ble/core/ble_defs.h \
+ ../../../../../../../Middlewares/ST/STM32_WPAN/ble/core/template/osal.h \
+ ../../../../../../../Middlewares/ST/STM32_WPAN/ble/core/template/compiler.h
+C:/Users/alexa/Downloads/en.stm32cubewb-v1-16-0/STM32Cube_FW_WB_V1.16.0/Middlewares/ST/STM32_WPAN/ble/core/auto/ble_gatt_aci.h:
+C:/Users/alexa/Downloads/en.stm32cubewb-v1-16-0/STM32Cube_FW_WB_V1.16.0/Middlewares/ST/STM32_WPAN/ble/core/auto/ble_types.h:
+../../../../../../../Middlewares/ST/STM32_WPAN/ble/core/template/ble_const.h:
+../../../../../../../Middlewares/ST/STM32_WPAN/ble/core/ble_std.h:
+../../../../../../../Middlewares/ST/STM32_WPAN/ble/core/ble_defs.h:
+../../../../../../../Middlewares/ST/STM32_WPAN/ble/core/template/osal.h:
+../../../../../../../Middlewares/ST/STM32_WPAN/ble/core/template/compiler.h:
---
+++ BLE_HeartRate/Debug/Middlewares/STM32_WPAN/osal.d
@@ -0,0 +1,4 @@
+Middlewares/STM32_WPAN/osal.o: \
+ C:/Users/alexa/Downloads/en.stm32cubewb-v1-16-0/STM32Cube_FW_WB_V1.16.0/Middlewares/ST/STM32_WPAN/ble/core/template/osal.c \
+ C:/Users/alexa/Downloads/en.stm32cubewb-v1-16-0/STM32Cube_FW_WB_V1.16.0/Middlewares/ST/STM32_WPAN/ble/core/template/osal.h
+C:/Users/alexa/Downloads/en.stm32cubewb-v1-16-0/STM32Cube_FW_WB_V1.16.0/Middlewares/ST/STM32_WPAN/ble/core/template/osal.h:
