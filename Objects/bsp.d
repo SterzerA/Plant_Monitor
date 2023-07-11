c1e59a7bce0501de2b1089373d3b1d5a6b31aead
SterzerA <alexander.w.sterzer@googlemail.com>
2023-07-11 13:48:02 +0200

    remove blinky example

--- Objects/bsp.d
+++
@@ -1,7 +0,0 @@
-./objects/bsp.o: bsp.c C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
-  stm32wb55xx.h core_cm4.h \
-  C:\Users\alexa\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_version.h \
-  C:\Users\alexa\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_compiler.h \
-  C:\Users\alexa\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_armclang.h \
-  C:\Users\alexa\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\mpu_armv7.h \
-  system_stm32wbxx.h bsp.h
